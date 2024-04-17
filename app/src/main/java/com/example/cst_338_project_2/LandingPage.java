0fb2444e64c10cc05985ae7770c3f7b94c45f04b
JoBarn24 <157662244+JoBarn24@users.noreply.github.com>
2024-04-17 11:07:58 -0700

    Created correct LandingPage activity

---
+++ app/src/main/java/com/example/cst_338_project_2/LandingPage.java
@@ -0,0 +1,14 @@
+package com.example.cst_338_project_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+    }
+}
