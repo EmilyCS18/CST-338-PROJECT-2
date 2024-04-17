8105170f2eb9bc45887f048ce047fbb1ce02623e
Emily <EmilyGC.0918@gmail.com>
2024-04-16 17:05:09 -0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "CST-338-PROJECT-2"
+include(":app")
