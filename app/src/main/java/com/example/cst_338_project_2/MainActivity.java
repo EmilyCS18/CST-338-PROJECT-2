4c6158359059a0ccd76d3302774d7bda63ea4f56
Emily <EmilyGC.0918@gmail.com>
2024-04-17 22:02:52 -0700

    Need to predefined users... Database is working for users who are signing up, but app crashes when user wants to login.

--- app/src/main/java/com/example/cst_338_project_2/MainActivity.java
+++ app/src/main/java/com/example/cst_338_project_2/MainActivity.java
@@ -3,13 +3,35 @@ package com.example.cst_338_project_2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    //random comment to make a commit
+   EditText username;
+   EditText password;
+   Button loginButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //username = findViewById(R.id.username);
+       // password = findViewById(R.id.password);
+        //loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("user") && password.getText().toString().equals("1234")) {
+                    Toast.makeText(MainActivity.this,"Login Successful YAY!!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this,"Login Failed!!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
